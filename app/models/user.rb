4a53228ef5d1c886860b7d4b03eb0c018f632c8e
shidiq22 <shidiq.pratama@gmail.com>
2023-07-18 21:06:48 +0700

    update the model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
-end
+    has_many :sleep_records
+  
+    has_many :follower_relationships, foreign_key: :followed_id, class_name: 'Friendship'
+    has_many :followers, through: :follower_relationships, source: :follower
+  
+    has_many :followed_relationships, foreign_key: :follower_id, class_name: 'Friendship'
+    has_many :followed_users, through: :followed_relationships, source: :followed
+  
+    def following
+      followed_users
+    end
+  end
+  
