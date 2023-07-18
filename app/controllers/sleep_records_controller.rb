2d6898abd58518af66b5dfa316bb23dd0c0a2f70
shidiq22 <shidiq.pratama@gmail.com>
2023-07-18 22:40:21 +0700

    add feature clock out

--- app/controllers/sleep_records_controller.rb
+++ app/controllers/sleep_records_controller.rb
@@ -1,9 +1,17 @@
 class SleepRecordsController < ApplicationController
-    # API endpoint for clocking in
+  # API endpoint for clocking in
   def clock_in
     user = User.find(params[:user_id])
     sleep_record = user.sleep_records.create(clock_in_time: Time.current)
     render json: sleep_record, status: :created
+  end
+
+  # API endpoint for clocking out
+  def clock_out
+    user = User.find(params[:user_id])
+    sleep_record = user.sleep_records.last
+    sleep_record.update(clock_out_time: Time.current)
+    render json: sleep_record, status: :ok
   end
 
   # API endpoint for retrieving all clocked-in times for a user
--- app/models/sleep_record.rb
+++ app/models/sleep_record.rb
@@ -1,3 +1,6 @@
 class SleepRecord < ApplicationRecord
   belongs_to :user
+
+  validates :clock_in_time, presence: true
+  validates :clock_out_time, presence: true
 end
